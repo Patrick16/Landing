f4eac7ccaaf248bb42a3c2255bd579c61fbd564a
Lev Kurbanov <lKurbanov@foss.dk>
2020-09-13 16:37:36 +0300

    added imafes and fonts. updated gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
+const rename = require("gulp-rename");
 
 /*------Server--------*/
 gulp.task('server', function() {
@@ -26,10 +27,11 @@ gulp.task('templates:compile', function buildHTML() {
 });
 
 /*----Style compile---*/
-gulp.task('sass', function () {
+gulp.task('styles:compile', function () {
     return gulp.src('source/styles/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('.build/css'));
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('build/css'));
 });
 
 /*-------Sprite------*/
@@ -65,3 +67,15 @@ gulp.task('copy:images', function () {
 
 /*---Copy---*/
 gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
+
+/*---Watchers----*/
+gulp.task('watch', function () {
+    gulp.watch('source/templates/**/*.pug',gulp.series('templates:compile'))
+    gulp.watch('source/styles/**/*.scss',gulp.series('styles:compile'));
+});
+
+gulp.task('default', gulp.series(
+    'clean',
+    gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+    gulp.parallel('watch', 'server')
+));
